cebe2a82761c795e381b850bcd51e735a493ab21
GitHub-Sanjit <sanjit.est14@gamil.com>
2024-05-18 09:19:20 +0600

    updated

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import SearchForm from "./SearchForm";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="weather-app-wrapper">
+          <div className="weather-app rounded-4">
+            <SearchForm defaultCity="sao paulo" />
+          </div>
+          <footer>
+            Open-source code by Sanjit on {""}
+            <a
+              href="https://github.com/GitHub-Sanjit"
+              target="_blank"
+            >
+              <strong>Github</strong>
+            </a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
